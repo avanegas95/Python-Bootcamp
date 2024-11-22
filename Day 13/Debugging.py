72768e4df42cabe42846648f6467af1a4a993c13
Anderson <avanegas95@gmail.com>
2024-11-21 23:04:41 -0500

    .DS_Store added to ignore

--- Day 13/Debugging.py
+++ Day 13/Debugging.py
@@ -1,7 +1,19 @@
-number = int(input("Which number do you want to check?"))
+# number = int(input("Which number do you want to check?"))
 
-if number % 2 == 0:
-  print("This is an even number.")
-else:
-  print("This is an odd number.")
+# if number % 2 == 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
   
+year = input("Which year do you want to check?")
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
